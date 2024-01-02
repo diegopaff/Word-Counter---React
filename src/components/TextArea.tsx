1b06574f450d4a3cf03ef2e255c97cfdb8bc2d60
diegopaff <difercuru@gmail.com>
2024-01-02 16:12:53 -0300

    feat: functionality and script tag check in textarea input

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 function TextArea() {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let newText: string = e.currentTarget.value;
+
+    if (newText.includes("<script>")) {
+      setShowWarning(true);
+      newText = newText.replace("<script>", "");
+    }
+
+    setText(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        className="textarea"
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+      />
+      {showWarning && <Warning />}
+    </div>
+  );
 }
 
 export default TextArea;
---
+++ src/components/Warning.tsx
@@ -0,0 +1,5 @@
+function Warning() {
+  return <p className="warning">You can't use the script tag</p>;
+}
+
+export default Warning;
